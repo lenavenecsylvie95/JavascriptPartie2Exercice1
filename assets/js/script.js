e296eaab206192ab037817052510a46e6c92c12c
Sylvie et Lhétie <sylvie@MacBook-Pro-de-Sylvie.local>
2020-05-06 20:57:04 +0200

    Exe1

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function setNewImage()
+{
+  document.getElementById('img1').src="assets/img/igor et grichka avant.jpg";
+}
+function setOldImage()
+{
+  document.getElementById('img1').src="assets/img/igor-et-grichka-bogdanov-2015-APRES.jpg";
+}
